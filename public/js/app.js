af393f1e174d9508ffd87e92bf2d3a9ee355a747
Jibbyworks <JDBucog@yahoo.com>
2021-11-19 12:50:39 -0800

    editabletimer, editabletimerlist, timerdashboard

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,52 @@
+class TimersDashboard extends React.Component {
+  render() {
+    return (
+      <div className="ui three column centered grid">
+        <div className="column">
+          <EditableTimerList />
+          <ToggleableTimerForm isOpen={true} />
+        </div>
+      </div>
+    )
+  }
+}
 
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+      <div className="timers">
+        <EditableTimer
+          title="Learn React"
+          project="Web Domination"
+          elapsed="8985600"
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title="Make reworked portfolio website in React"
+          project="Get a Job"
+          elapsed="8123122"
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    )
+  }
+}
+
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return <TimerForm title={this.props.title} project={this.props.project} />
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      )
+    }
+  }
+}
